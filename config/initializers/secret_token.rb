dd5b1afe16a9467b17a147727285d8834a30fd49
marmit <mecrobmarmit@yahoo.com>
2012-06-04 01:13:49 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamSergio::Application.config.secret_token = 'd9a8b8e32aa132226ba25ffa4e50a6fa434d7edf64bd274020eaf2a03373d5e7db5e73e251d1bd2212b3bde71bc6d65edf7389e5cf8953320ade4e64e1bf9f8c'
